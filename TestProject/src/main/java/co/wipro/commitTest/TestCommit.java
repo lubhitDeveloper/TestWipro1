70d4f18a7dfb0ec2b736e625cd60c265f473258e
LUBHIT MALHOTRA <lubhitmalhotra12@gmail.com>
2022-03-22 15:39:55 +0530

    Added first commit message

---
+++ TestProject/src/main/java/co/wipro/commitTest/TestCommit.java
@@ -0,0 +1,10 @@
+package co.wipro.commitTest;
+
+public class TestCommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First commit practice");
+	}
+
+}
